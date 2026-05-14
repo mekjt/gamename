341109c38538bc3fd3012569570f8405861027b1
mekjt <gugjunghwa2@gmail.com>
2026-05-14 17:29:06 +0900

    gamename

---
+++ Assets/forder/user.cs
@@ -0,0 +1,19 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class user : MonoBehaviour 
+{
+    void Update() 
+    {
+        Console.WriteLine("이동중");
+    }
+
+    class Players
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
