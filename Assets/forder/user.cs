7ce930079de55d098cba2f52dc398f0d6e3683b7
mekjt <gugjunghwa2@gmail.com>
2026-05-15 16:56:17 +0900

    gamename7

--- Assets/forder/user.cs
+++ Assets/forder/user.cs
@@ -1,23 +1,19 @@
 using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class user : MonoBehaviour
 {
     void Update()
-    {
-        Debug.Log("이동중");
-        //Console.WriteLine("dlehdwnd2");
-        Vector2 vec = new Vector2(1,1);//1번이 x값 2번이 y값 추정
-        transform.Translate(vec);
-    }
-
-    void start()
-    {
-        Debug.Log("이동중2");
+    { 
         Input.GetKeyDown(KeyCode.D);
+        {
+            Debug.Log("이동중");
+            Vector2 vec = new Vector2(1, 1);//1번이 x값 2번이 y값 추정
+            transform.Translate(vec);
+        }
     }
-
     void Start()
     {
        
