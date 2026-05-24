a0acce20a7865df65503d1d5bfa38a7e879ecb94
mekjt <gugjunghwa2@gmail.com>
2026-05-25 02:21:02 +0900

    gamename16
    추가 기능:달리기

--- Assets/scripts/user1.cs
+++ Assets/scripts/user1.cs
@@ -7,6 +7,7 @@ public class asdf : MonoBehaviour
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float jumpForce = 5.0f;
     [SerializeField] float speed = 5.0f;
+    [SerializeField] float upspeed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,22 +22,23 @@ public class asdf : MonoBehaviour
             Vector2 jumpDir = new Vector2(0, jumpForce);
             rb.AddForce(jumpDir, ForceMode2D.Impulse);
         }
-        Movement();
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-
-
-        }
+            Movement();
     }
 
     void Movement()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
+        float h = Input.GetAxis("Horizontal");//가로가 맞는데?????  x값이고
+        float v = Input.GetAxis("Vertical");//세로고 
+        float x = speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            x = speed + upspeed;
+        }
+        rb.linearVelocity = new Vector2(h * x, rb.linearVelocity.y);
         Vector2 velocity = new Vector2(h, v).normalized;
-        //rb.linearVelocity = velocity * speed;
-        // Movement() 내부 수정 예시 (X축만 내 입력 적용, Y축은 물리 엔진에 위임)
-        rb.linearVelocity = new Vector2(h * speed, rb.linearVelocity.y);
+        //rb.velocity = velocity * speed;
+        // Movement() 내부 수정 예시 (X축만 입력 적용, Y축은 물리 엔진에 위임)
+        //rb.linearVelocity = new Vector2(h * speed, rb.linearVelocity.y);
+        // rb.velocity = new Vector2(h * speed, -upspeed);
     }
 }
