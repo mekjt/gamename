b213b7c6eabe07082448353e8b42ce2f5281cbc6
mekjt <gugjunghwa2@gmail.com>
2026-05-25 17:29:22 +0900

    gamename17
    코인 디자인 추가

---
+++ Assets/scripts/coin.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+
+public class coin : MonoBehaviour
+{
+    public int new_coin;
+
+    public bool isGet = false;//획득 확인
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    { 
+     if (Collision.CompareTag("player") && !isGet)
+        {
+            isGet = true;//획득
+            bool coinAdded = GetItem(collision);
+
+            if (coinAdded)
+            {
+                Hideitem();
+            }
+            else 
+            {
+                isGet = false;
+            }
+                }
+    }
+}
